560f1bd9b7ddaf20095f8385c9643163a324c72f
aT <atif.siddiqui2@topcoder.com>
2021-09-06 12:28:02 +0530

    Change path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-app.use('/challenges',
+app.use('/challenges-app',
   express.static("./dist", {
     setHeaders: function setHeaders(res) {
       res.header("Access-Control-Allow-Origin", "*");
