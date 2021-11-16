c5a1b0c7f5c65d18e364f526190744e216e1e1e2
M Fikri A <fikzzzy@gmail.com>
2021-11-16 17:19:42 +0700

    linter

--- src/utils/index.js
+++ src/utils/index.js
@@ -170,11 +170,11 @@ export function parseTotalPrizes(s) {
   if (valid) return n;
 }
 
-export function triggerDownload(fileName,blob) {
+export function triggerDownload(fileName, blob) {
   const url = window.URL.createObjectURL(new Blob([blob]));
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = url;
-  link.setAttribute('download', fileName);
+  link.setAttribute("download", fileName);
   document.body.appendChild(link);
   link.click();
   link.parentNode.removeChild(link);
