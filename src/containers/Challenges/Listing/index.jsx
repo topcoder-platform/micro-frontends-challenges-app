63375f96594550fabcce6934ad06a65afe5de288
pauline7 <pkanaiza@gmail.com>
2021-12-01 18:18:58 +0300

    Issue 211 Added Trash Icon to Search Bar

--- src/containers/Challenges/Listing/index.jsx
+++ src/containers/Challenges/Listing/index.jsx
@@ -14,6 +14,7 @@ import * as utils from "../../../utils";
 
 import * as constants from "../../../constants";
 import IconSearch from "../../../assets/icons/search.svg";
+import IconTrash from "../../../assets/icons/IconTrashSimple.svg";
 import "./styles.scss";
 
 const Listing = ({
@@ -70,6 +71,20 @@ const Listing = ({
               }}
               maxLength="100"
             />
+            <span styleName="trash-icon">
+              <IconTrash
+                size="xs"
+                onClick={() => {
+                  onSearch.current(() => {
+                    const filterChange = {
+                      search: "",
+                      page: 1,
+                    };
+                    updateFilter(filterChange);
+                  });
+                }}
+              />
+            </span>
           </div>
           <div styleName="separator" />
           <div
