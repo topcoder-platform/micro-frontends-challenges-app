47dacaaf9df2eb5be3e66f6edb1d168a2007b0fe
LieutenantRoger <gair_2015@hotmail.com>
2021-12-03 00:47:46 +0800

    clear btn

--- src/containers/Challenges/Listing/index.jsx
+++ src/containers/Challenges/Listing/index.jsx
@@ -14,6 +14,7 @@ import * as utils from "../../../utils";
 
 import * as constants from "../../../constants";
 import IconSearch from "../../../assets/icons/search.svg";
+import IconClear from "../../../assets/icons/close-gray.svg";
 import Button from "../../../components/Button";
 
 import "./styles.scss";
@@ -77,6 +78,24 @@ const Listing = ({
               }}
               maxLength="100"
             />
+            {search.length ? (
+              <span styleName="clear-icon">
+                <IconClear
+                  size="xs"
+                  onClick={() => {
+                    onSearch.current(() => {
+                      const filterChange = {
+                        search: "",
+                        page: 1,
+                      };
+                      updateFilter(filterChange);
+                    });
+                  }}
+                />
+              </span>
+            ) : (
+              <span type="hidden"></span>
+            )}
           </div>
           <div styleName="separator" />
           <div
